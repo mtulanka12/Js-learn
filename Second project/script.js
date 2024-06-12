8e60a5c7e8740fa5b1008b3932d66b78b3ed90c3
Moshan Wijenayaka <mtulanka@gmail.com>
2024-06-12 18:14:27 +0600

    js functions
    Refreshing functions

---
+++ Second project/script.js
@@ -0,0 +1,17 @@
+function loger()
+{
+    console.log('xdcc');
+}
+
+loger();
+
+let val1=1;
+let val2=1;
+
+function sum(val1,val2)
+{
+    val1 =parseInt(val1)+parseInt(val2);
+    console.log(val1);
+}
+
+sum(val1,val2);
