2eb64d993fdbaf55008d7eddd96f99054a35880c
Moshan Wijenayaka <mtulanka@gmail.com>
2024-06-12 20:27:25 +0600

    Arrow function basics
    create arrow function with multiple parameter values.

---
+++ Second project/Arrow-functions/arrow_script.js
@@ -0,0 +1,10 @@
+//Arrow functions
+
+
+const calAge3 = birthYear => 2024-birthYear; //Crate function
+const age3 =calAge3(1998); //passing value 
+console.log(age3); //calling function
+
+const test_function = (test_param_1, test_param_2) => (10 + test_param_1+test_param_2);
+const awnser = test_function(1,1);
+console.log(awnser);
